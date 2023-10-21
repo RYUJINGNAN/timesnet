8c4bcc02d620dd9aaaf686d6b86fc7b6f5292d89
RYUJINGNAN <ljn080800@outlook.com>
2023-10-21 11:54:42 +0900

    update timesnet

---
+++ scripts/anomaly_detection/PSM/TimesNet.sh
@@ -0,0 +1,23 @@
+export CUDA_VISIBLE_DEVICES=0
+
+python -u run.py \
+  --task_name anomaly_detection \
+  --is_training 1 \
+  --root_path /home/ubuntu22/Time-Series-Library-main/datasets/PSM \
+  --model_id PSM \
+  --model TimesNet \
+  --data PSM \
+  --features M \
+  --seq_len 100 \
+  --pred_len 0 \
+  --d_model 64 \
+  --d_ff 64 \
+  --e_layers 2 \
+  --enc_in 25 \
+  --c_out 25 \
+  --top_k 3 \
+  --anomaly_ratio 1 \
+  --batch_size 128 \
+  --train_epochs 3 \
+  --device 0 \
+  --use_gpu True
